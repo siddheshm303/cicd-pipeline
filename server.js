461dd34dea24a28bfee364f692df700ab42cbed7
siddheshm303 <siddheshm303@gmail.com>
2025-10-23 23:32:12 +0530

    changed commit

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/',(req,res) => {
     res.json({
         status: 200,
-        message:"Hello World"
+        message:"Hello Bharat"
     })
 })
 
